fb788b7f71a4dc17b66cf89a137a4c5797f76e4c
Torsten Kurbad <t.kurbad@iwm-kmrc.de>
2017-02-10 15:27:10 +0100

    Merged upstream/edge

--- src/modules/tools/temperaturecontrol/max31855.cpp
+++ src/modules/tools/temperaturecontrol/max31855.cpp
@@ -70,7 +70,6 @@ float Max31855::get_temperature()
     float sum = 0;
     for (int i=0; i<readings.size(); i++)
         sum += *readings.get_ref(i);
-    }
 
     return sum / readings.size();
 }
