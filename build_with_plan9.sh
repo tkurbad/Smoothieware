7739e8b30a032fca2199d3a62d363c730c1240e7
Torsten Kurbad <t.kurbad@iwm-kmrc.de>
2018-10-08 14:50:23 +0200

    Updated build script

--- build_with_plan9.sh
+++ build_with_plan9.sh
@@ -1,1 +1,10 @@
-make PLAN9=1 clean all
+#! /usr/bin/env bash
+# Modify next line and set destination drive to match mbed device
+export LPC_DEPLOY='cp PROJECT.bin /media/MBED/ ; sync'
+
+SCRIPT_PATH=$0
+SCRIPT_PATH=${SCRIPT_PATH%/*}
+cd $SCRIPT_PATH
+SCRIPT_PATH=$PWD
+export PATH=$SCRIPT_PATH/gcc-arm-none-eabi/bin:$PATH
+exec bash -c "make clean ; make PLAN9=1 all"
